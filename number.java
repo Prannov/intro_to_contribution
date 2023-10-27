780f98d78a2ae0e080c8f8ad216b8d3acba80053
rayypan <sandipanray285@gmail.com>
2023-10-28 01:03:09 +0530

    "Numbers that are divisible by 3"

---
+++ number.java
@@ -0,0 +1,12 @@
+class number
+{
+    public static void main()
+    {
+        System.out.println("Numbers from 1-10 which are divisible by 3 are:");
+        for(int i=0;i<=10;i++)
+        {
+            if(i%3==0)
+            System.out.println(i+" ");
+        }
+    }
+}
