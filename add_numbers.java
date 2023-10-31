3468ca62458ec8493560626ae102b3986e0e5ee6
RitvikMalladi <135348833+RitvikMalladi@users.noreply.github.com>
2023-10-31 23:54:35 +0530

    Add files via upload

---
+++ add_numbers.java
@@ -0,0 +1,21 @@
+//question 1
+ import java.util.Scanner;
+public class add_numbers {
+    public static void main(String[] args){
+
+        float a = 7/4*9/2;
+        System.out.println(a);
+    }
+}
+//        int a = 100;
+//        int b = 200;
+//        int c = 300;
+//        int sum = a + b + c;
+////        System.out.println(sum);
+//        System.out.println("what is your name ?");
+//        Scanner sc = new Scanner(System.in);
+//        String name = sc.next();
+//        System.out.println("good morning " + name + "have a good day!!");
+//
+//
+
