544947fb3e341cddf23d16ce11903477a336ea91
utpalaashivasai <149525529+utpalaashivasai@users.noreply.github.com>
2023-10-31 23:36:42 +0530

    Create constructor_in_inheritence.java

---
+++ constructor_in_inheritence.java
@@ -0,0 +1,30 @@
+class shiva{
+    int a;
+    int b;
+    public shiva(int myA,int myB){
+        myA=a;
+        myB=b;
+    }
+
+
+}
+//class sai extends shiva{
+ //   int d;
+
+  //  public sai(int myD){
+//        super(a,b);
+       // myD=d;
+ //   }
+
+
+
+
+//}
+public class con_inherit {
+    public static void main(String[] args) {
+        shiva s=new shiva(12,12);
+        System.out.println(s.a);
+
+    }
+}
+
